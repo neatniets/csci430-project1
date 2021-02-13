6457415faf3c99d957a257a1f9e4422b3d98952f
niets <mymailboxrebelled@gmail.com>
2021-02-13 17:14:13 -0600

    Create warehouse class

---
+++ Warehouse.java
@@ -0,0 +1,227 @@
+import java.util.*;
+import java.io.*;
+public class Warehouse implements Serializable {
+        private static final long serialVersionUID = 1L;
+
+        /** List of all products stocked by warehouse. */
+        private ProductList plist;
+        /** List of all clients tracked by warehouse. */
+        private ClientList clist;
+        /** List of all active suppliers for the warehouse. */
+        private SupplierList slist;
+        private static Warehouse warehouse = null;
+        private Warehouse() {
+                plist = new ProductList();
+                clist = ClientList.instance();
+                slist = new SupplierList();
+        }
+        public static Warehouse instance() {
+                /*if (warehouse == null) {
+                        MemberIdServer.instance(); // instantiate all singletons
+                        return (warehouse = new Warehouse());
+                } else {
+                        return warehouse;
+                }*/
+                if (warehouse == null) {
+                        warehouse = new Warehouse();
+                }
+                return warehouse;
+        }
+
+        /** Add a product to warehouse.
+         * @param[in]   product_id      unique identifier for product
+         * @post        will fail if product_id is not unique
+         * @param[in]   name            name of product
+         * @param[in]   price           price of product
+         * @param[in]   quantity        quantity of product
+         * @return      reference to Product object on success,
+         *              null on failure */
+        public Product addProduct(int product_id,
+                                  String name,
+                                  double price,
+                                  int quantity) {
+                /* create product */
+                Product p = new Product(product_id, name, "", price, quantity);
+                /* add product */
+                if (plist.add(p)) { // success
+                        return p;
+                } else { // fail
+                        return null;
+                }
+        }
+        /** Get an iterator for all products.
+         * @return      the iterator */
+        public Iterator<Product> getProducts() {
+                return plist.iterator();
+        }
+        /** Get a specific product.
+         * @param       pid     ID of product
+         * @return      product object with that ID on success,
+         *              null if the product doesn't exist */
+        public Product getProduct(int pid) {
+                return plist.find(pid);
+        }
+        /** Update the price of a product.
+         * @param       pid     ID of product to update
+         * @param       price   price to change product to
+         * @post        returned product object should be checked to see if
+         *              price successfully changed
+         * @return      product object found,
+         *              null if product could not be found */
+        public Product updateProductPrice(int pid,
+                                          double price) {
+                /* try to find product */
+                Product p = plist.find(pid);
+                if (p == null) { // no product found
+                        return null;
+                }
+                p.set_price(price);
+                return p;
+        }
+
+        public Supplier addSupplier(int supplier_id,
+                                    String name,
+                                    String address,
+                                    int phone) {
+                /* create supplier */
+                Supplier s = new Supplier(supplier_id, name, address, phone);
+                /* add supplier */
+                if (slist.add(s)) { // success
+                        return s;
+                } else { // fail
+                        return null;
+                }
+        }
+        public Iterator<Supplier> getSuppliers() {
+                return slist.iterator();
+        }
+        public Supplier getSupplier(int sid) {
+                return slist.find(sid);
+        }
+        public Supplier updateSupplierInfo(int sid,
+                                           String name,
+                                           String address,
+                                           int phone) {
+                /* try to find supplier */
+                Supplier s = slist.find(sid);
+                if (s == null) { // no supplier found
+                        return null;
+                }
+                s.setName(name);
+                s.setAddress(address);
+                s.setPhone(phone);
+                return s;
+        }
+
+        /* Matt got a working ID generator */
+        public Client addClient(String name,
+                                String address,
+                                String phone) {
+                /* create client */
+                Client c = new Client(name, address, phone);
+                /* add client */
+                if (clist.insertClient(c)) { // success
+                        return c;
+                } else { // fail
+                        return null;
+                }
+        }
+        public Iterator<Client> getClients() {
+                return clist.getClients();
+        }
+        public Client getClient(String client_id) {
+                return clist.getClient(client_id);
+        }
+        public Client updateClientInfo(String client_id,
+                                       String name,
+                                       String address,
+                                       String phone) {
+                /* try to find client */
+                Client c = clist.getClient(client_id);
+                if (c == null) { // no client found
+                        return null;
+                }
+                c.setName(name);
+                c.setAddress(address);
+                c.setPhone(phone);
+                return c;
+        }
+
+        public boolean add2ShoppingCart(String client_id,
+                                        int product_id) {
+                /* find client */
+                Client c = clist.getClient(client_id);
+                if (c == null) { // client not found
+                        return false;
+                }
+                /* find product */
+                Product p = plist.find(product_id);
+                if (p == null) { // product not found
+                        return false;
+                }
+                /* add product to cart */
+                return c.getShoppingCart().insertProduct(p);
+        }
+        public Iterator<Product> getCartContents(String client_id) {
+                /* try to find client */
+                Client c = clist.getClient(client_id);
+                if (c == null) { // client not found
+                        return null;
+                }
+                /* return cart's iterator */
+                return c.getShoppingCart().getProducts();
+        }
+
+        public static Warehouse retrieve() {
+                try {
+                        FileInputStream file = new FileInputStream("WarehouseData");
+                        ObjectInputStream input = new ObjectInputStream(file);
+                        input.readObject();
+                        //MemberIdServer.retrieve(input);
+                        return warehouse;
+                } catch(IOException ioe) {
+                        ioe.printStackTrace();
+                        return null;
+                } catch(ClassNotFoundException cnfe) {
+                        cnfe.printStackTrace();
+                        return null;
+                }
+        }
+        public static  boolean save() {
+                try {
+                        FileOutputStream file = new FileOutputStream("WarehouseData");
+                        ObjectOutputStream output = new ObjectOutputStream(file);
+                        output.writeObject(warehouse);
+                        //output.writeObject(MemberIdServer.instance());
+                        return true;
+                } catch(IOException ioe) {
+                        ioe.printStackTrace();
+                        return false;
+                }
+        }
+        private void writeObject(java.io.ObjectOutputStream output) {
+                try {
+                        output.defaultWriteObject();
+                        output.writeObject(warehouse);
+                } catch(IOException ioe) {
+                        System.out.println(ioe);
+                }
+        }
+        private void readObject(java.io.ObjectInputStream input) {
+                try {
+                        input.defaultReadObject();
+                        if (warehouse == null) {
+                                warehouse = (Warehouse) input.readObject();
+                        } else {
+                                input.readObject();
+                        }
+                } catch(IOException ioe) {
+                        ioe.printStackTrace();
+                } catch(Exception e) {
+                        e.printStackTrace();
+                }
+        }
+        public String toString() {
+                return plist + "\n" + clist + "\n" + slist + "\n";
+        }
+}
