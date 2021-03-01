8e8719fd2417402e57972dab97e2391aeb39c525
niets <mymailboxrebelled@gmail.com>
2021-03-01 13:58:51 -0600

    Create Supply

---
+++ Supply.java
@@ -0,0 +1,26 @@
+public class Supply {
+	private String supplier_id;
+	private String product_id;
+	private double price;
+	
+	public void Supply(){
+	}
+	
+	public void Supply(String supplier_id, String product_id, double price){
+		this.supplier_id = supplier_id;
+		this.product_id = product_id;
+		this.price = price;
+	}
+	
+	public String getSupplierID(){
+		return supplier_id;
+	}
+	
+	public String getProductID(){
+		return product_id;
+	}
+	
+	public double getPrice(){
+		return price;
+	}
+}
