bc9f1b2ec574fe2d219e1a85995dd4ebc15ee5fc
niets <mymailboxrebelled@gmail.com>
2021-03-02 13:38:05 -0600

    Use Product object in CartItem
    The Product object needs to be accessible via the Client for the
    processOrder function. Using a product ID instead of an object reference
    would work fine if we had a way to access the Warehouse object to search
    for the Product, however a way to access the Warehouse object is not
    currently implemented and probably won't change, so this will change
    instead.

--- CartItem.java
+++ CartItem.java
@@ -2,23 +2,23 @@ import java.util.*;
 import java.io.*;
   public class CartItem {
   private String quantity;
-  private String Productid;
-  public CartItem(String Productid, String quantity) {
-    this.Productid= Productid;
+  private Product product;
+  public CartItem(Product product, String quantity) {
+    this.product = product;
     this.quantity = quantity;
   }
-  public String getquantity() {
+  public String getQuantity() {
     return quantity;
   }
 
-  public String getProductId() {
-    return Productid;
+  public Product getProduct() {
+    return product;
   }
 
-  public void setquantity(String newquantity) {
+  public void setQuantity(String newquantity) {
     quantity = newquantity;
   }
-  public void setproductid(String newProductid) {
-    Productid = newProductid;
+  public void setproductid(Product new_product) {
+          product = new_product;
   }
 }
