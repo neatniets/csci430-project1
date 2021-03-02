c6e7f9889d3c8a6ba48cd7d0b1b05208e6be353e
niets <mymailboxrebelled@gmail.com>
2021-03-02 13:24:37 -0600

    Implement CartItem

--- CartItem.java
+++ CartItem.java
@@ -1,10 +1,24 @@
-public class CartItem {
-  public Product getProduct() {
-    /* dummy */
-    return new Product("");
+import java.util.*;
+import java.io.*;
+  public class CartItem {
+  private String quantity;
+  private String Productid;
+  public CartItem(String Productid, String quantity) {
+    this.Productid= Productid;
+    this.quantity = quantity;
   }
-  public int getQuantity() {
-    /* dummy */
-    return 0;
+  public String getquantity() {
+    return quantity;
+  }
+
+  public String getProductId() {
+    return Productid;
+  }
+
+  public void setquantity(String newquantity) {
+    quantity = newquantity;
+  }
+  public void setproductid(String newProductid) {
+    Productid = newProductid;
   }
 }
